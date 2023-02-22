b78c78fc3186c6e124694ab7460566fa361c280f
No Sleep <49619557+nosleep77@users.noreply.github.com>
2023-02-22 09:27:06 -0500

    Update Calculator.java

--- src/main/java/com/leszko/calculator/Calculator.java
+++ src/main/java/com/leszko/calculator/Calculator.java
@@ -3,8 +3,9 @@ package com.leszko.calculator;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
-/* test 
- * test */
+/* 
+* comment for class
+*/
 @Service
 public class Calculator {
         final static int umlNUMBER1 = 3;
