3630dbf5ae5deab32bbbb8ed4c33d10ec1dffa97
No Sleep <nosleep@example.com>
2023-02-22 00:49:43 -0500

    project

--- src/main/java/com/leszko/calculator/Calculator.java
+++ src/main/java/com/leszko/calculator/Calculator.java
@@ -3,7 +3,8 @@ package com.leszko.calculator;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
-/* test */
+/* test 
+ * test */
 @Service
 public class Calculator {
         final static int umlNUMBER1 = 3;
